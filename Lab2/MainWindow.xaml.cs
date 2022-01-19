9d4aa50eed45f81458a98f1268fcbfeb27c61f9a
Ilya Grigoryev <ilya.g@fx.gl>
2022-01-19 22:41:11 +0300

    Докоммит части проекта

---
+++ Lab2/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Lab2.BlackMagic;
+using Microsoft.Win32;
+
+namespace Lab2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Automata.Automata Automata { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private async void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            AutomataInfo.Text = "";
+            
+            if (openFileDialog.ShowDialog() == true)
+            {
+                var config = await File.ReadAllTextAsync(openFileDialog.FileName);
+                Automata = new Automata.Automata(config);
+                AppendAutomataInfo(Automata.ParsingErrors);
+                
+                if(string.IsNullOrEmpty(Automata.ParsingErrors))
+                {
+                    await UpdateAutomataInfo();
+                }
+            }
+        }
+
+        private async void ExpressionEnteredHandler(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return && !string.IsNullOrEmpty(AutomataExpression.Text) && Automata != null)
+            {
+                AppendAutomataInfo($"Entered expression: {AutomataExpression.Text}");
+                try
+                {
+                    AppendAutomataInfo(Automata.ParseExpression(AutomataExpression.Text)
+                        ? $"Expression successfully parsed with final state {Automata.CurrentState.Name}"
+                        : $"Expression parsing failed with state {Automata.CurrentState.Name}");
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception);
+                    AppendAutomataInfo(exception.Message);
+                }
+            }
+        }
+
+        private void AppendAutomataInfo(string info)
+        {
+            if (!string.IsNullOrEmpty(info))
+            {
+                AutomataInfo.Text += $"\n{info}";
+            }
+        }
+
+        private async void btnToDfa_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (Automata == null)
+            {
+                AutomataInfo.Text = "Provide automata config first";
+                return;
+            }
+            
+            Automata.ToDfa();
+            await UpdateAutomataInfo();
+        }
+
+        private async Task UpdateAutomataInfo()
+        {
+            var result = await SnakeWizard.CastMagic(Automata.States);
+            AutomataGraph.Source = result;
+            
+            AutomataInfo.Text = "";
+            AppendAutomataInfo(Automata.GetAutomataInfo());
+            AppendAutomataInfo(Automata.GetConfig());
+        }
+    }
+}
